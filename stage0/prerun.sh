4483fb786e579a059648b0c4e089bac33ad5092d
Justin Partain <jpartain89@gmail.com>
2020-01-09 04:27:51 -0600

    Updates to prerun.sh

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -2,4 +2,14 @@
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	bootstrap buster "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+    bootstrap buster "${ROOTFS_DIR}" http://archive.raspbian.org/raspbian
+    bootstrap buster-staging "${ROOTFS_DIR}" http://archive.raspbian.org/raspbian
+
+    bootstrap buster "${ROOTFS_DIR}" http://archive.raspberrypi.org/debian
+
+    bootstrap buster "${ROOTFS_DIR}" http://ftp.debian.org/debian
+    bootstrap buster-updates "${ROOTFS_DIR}" http://ftp.debian.org/debian
+    bootstrap buster-backports "${ROOTFS_DIR}" http://ftp.debian.org/debian
+
+    bootstrap buster/updates "${ROOTFS_DIR}" http://security.debian.org/
 fi
